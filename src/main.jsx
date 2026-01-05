ddc381aa9ed42b9301a6c228ecc364a733cee629
Robel <robelamare75@gmail.com>
2026-01-05 20:45:03 +0300

    navbar is added

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {BrowserRouter as Router} from "react-router-dom"
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Router>
+       <App/>
+    </Router>
+   
+  </StrictMode>,
+)
